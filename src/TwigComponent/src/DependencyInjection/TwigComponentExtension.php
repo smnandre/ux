892e45ec00c3d7f0f9a224859d3f300d11b93216
Simon André <smn.andre@gmail.com>
2023-09-30 00:12:47 +0200

    Add tests

--- src/TwigComponent/src/DependencyInjection/TwigComponentExtension.php
+++ src/TwigComponent/src/DependencyInjection/TwigComponentExtension.php
@@ -11,14 +11,16 @@
 
 namespace Symfony\UX\TwigComponent\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Argument\AbstractArgument;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Exception\LogicException;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Parameter;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Stopwatch\Stopwatch;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 use Symfony\UX\TwigComponent\Command\ComponentDebugCommand;
 use Symfony\UX\TwigComponent\ComponentFactory;
@@ -26,9 +28,7 @@ use Symfony\UX\TwigComponent\ComponentRenderer;
 use Symfony\UX\TwigComponent\ComponentRendererInterface;
 use Symfony\UX\TwigComponent\ComponentStack;
 use Symfony\UX\TwigComponent\ComponentTemplateFinder;
-use Symfony\UX\TwigComponent\DataCollector\TwigComponentDataCollector;
 use Symfony\UX\TwigComponent\DependencyInjection\Compiler\TwigComponentPass;
-use Symfony\UX\TwigComponent\EventListener\TwigComponentLoggerListener;
 use Symfony\UX\TwigComponent\Twig\ComponentExtension;
 use Symfony\UX\TwigComponent\Twig\ComponentLexer;
 use Symfony\UX\TwigComponent\Twig\TwigEnvironmentConfigurator;
@@ -44,6 +44,8 @@ final class TwigComponentExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../../config'));
+
         if (!isset($container->getParameter('kernel.bundles')['TwigBundle'])) {
             throw new LogicException('The TwigBundle is not registered in your application. Try running "composer require symfony/twig-bundle".');
         }
@@ -109,25 +111,8 @@ final class TwigComponentExtension extends Extension
             ->addTag('console.command')
         ;
 
-        $container->register('ux.twig_component.component_logger_listener', TwigComponentLoggerListener::class)
-            ->addTag('kernel.event_subscriber');
-        if ($container->hasParameter('kernel.debug') && $container->getParameter('kernel.debug')) {
-            $container->register('ux.twig_component.logger_listener', TwigComponentLoggerListener::class)
-                ->setArguments([
-                    new Reference('debug.stopwatch', ContainerInterface::IGNORE_ON_INVALID_REFERENCE),
-                ])
-                ->addTag('kernel.event_subscriber');
-
-            $container->register('ux.twig_component.data_collector', TwigComponentDataCollector::class)
-                ->setArguments([
-                    new Reference('ux.twig_component.logger_listener'),
-                    new Reference('twig'),
-                ])
-                ->addTag('data_collector', [
-                    'template' => '@TwigComponent/Collector/twig_component.html.twig',
-                    'id' => 'twig_component',
-                    'priority' => 256,
-                ]);
+        if ($container->getParameter('kernel.debug') && class_exists(Stopwatch::class)) {
+            $loader->load('debug.php');
         }
     }
 }
