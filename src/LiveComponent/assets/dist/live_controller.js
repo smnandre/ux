1ea0824b25cea514edb351f385ef79d6e2d9357f
Simon André <smn.andre@gmail.com>
2024-02-29 23:10:00 +0100

    Build live_controller.js

--- src/LiveComponent/assets/dist/live_controller.js
+++ src/LiveComponent/assets/dist/live_controller.js
@@ -588,7 +588,7 @@ var Idiomorph = (function () {
          * @returns {boolean}
          */
         function ignoreValueOfActiveElement(possibleActiveElement, ctx) {
-            return ctx.ignoreActiveValue && possibleActiveElement === document.activeElement;
+            return ctx.ignoreActiveValue && possibleActiveElement === document.activeElement && possibleActiveElement !== document.body;
         }
 
         /**
