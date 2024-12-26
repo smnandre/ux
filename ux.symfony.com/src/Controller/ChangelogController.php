d4cdba5162662b72092adbe3fac4f9fa75c9ff4a
Simon André <smn.andre@gmail.com>
2024-12-26 03:16:12 +0100

    changelog turbo test

--- ux.symfony.com/src/Controller/ChangelogController.php
+++ ux.symfony.com/src/Controller/ChangelogController.php
@@ -13,6 +13,7 @@ namespace App\Controller;
 
 use App\Service\Changelog\ChangelogProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -29,6 +30,34 @@ class ChangelogController extends AbstractController
         $changelog = $this->changeLogProvider->getChangelog();
 
         return $this->render('changelog.html.twig', [
+            'changelog' => $changelog,
+        ]);
+    }
+    
+    #[Route('/changelog/{version}', name: 'app_changelog_version')]
+    public function tag(Request $request): Response
+    {
+        $changelog = $this->changeLogProvider->getChangelog();
+        
+        foreach ($changelog as $version) {
+            if ($version['version'] === $request->get('version')) {
+                break;
+            }
+            $version = null;
+        }
+        
+        if (!isset($version)) {
+            throw $this->createNotFoundException('Version not found');
+        }
+        
+        if ($turboFrame = $request->headers->has('Turbo-Frame')) {
+            return $this->renderBlock('changelog/changelog_version.html.twig', 'version', [
+                'version' => $version,
+            ]);
+        }
+        
+        return $this->render('changelog/changelog_version.html.twig', [
+            'version' => $version,
             'changelog' => $changelog,
         ]);
     }
