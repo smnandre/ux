afb69d97c04335e50130b61f2380b5a2be3aed0f
Simon André <smn.andre@gmail.com>
2025-05-08 04:12:10 +0200

    [Live] Fix some test namespaces

--- src/LiveComponent/tests/Integration/Twig/LiveComponentRuntimeTest.php
+++ src/LiveComponent/tests/Integration/Twig/LiveComponentRuntimeTest.php
@@ -43,4 +43,28 @@ final class LiveComponentRuntimeTest extends KernelTestCase
             ->assertSee('Prop3: howdy')
         ;
     }
+
+    public function testGetLiveAction(): void
+    {
+        $runtime = self::getContainer()->get('ux.live_component.twig.component_runtime');
+        \assert($runtime instanceof LiveComponentRuntime);
+
+        $props = $runtime->liveAction('action-name');
+        $this->assertSame('data-action="live#action" data-live-action-param="action-name"', $props);
+
+        $props = $runtime->liveAction('action-name', ['prop1' => 'val1', 'someProp' => 'val2']);
+        $this->assertSame('data-action="live#action" data-live-prop1-param="val1" data-live-some-prop-param="val2" data-live-action-param="action-name"', $props);
+
+        $props = $runtime->liveAction('action-name', ['prop1' => 'val1', 'prop2' => 'val2'], ['debounce' => 300]);
+        $this->assertSame('data-action="live#action" data-live-prop1-param="val1" data-live-prop2-param="val2" data-live-action-param="debounce(300)|action-name"', $props);
+
+        $props = $runtime->liveAction('action-name:prevent', ['pro1' => 'val1', 'prop2' => 'val2'], ['debounce' => 300]);
+        $this->assertSame('data-action="live#action:prevent" data-live-pro1-param="val1" data-live-prop2-param="val2" data-live-action-param="debounce(300)|action-name"', $props);
+
+        $props = $runtime->liveAction('action-name:prevent', [], ['debounce' => 300]);
+        $this->assertSame('data-action="live#action:prevent" data-live-action-param="debounce(300)|action-name"', $props);
+
+        $props = $runtime->liveAction('action-name', [], [], 'keydown.esc');
+        $this->assertSame('data-action="keydown.esc->live#action" data-live-action-param="action-name"', $props);
+    }
 }
