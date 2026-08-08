8adf225bb65c6a2ecf4c7cf2b0640aeec1a3c199
Simon André <smn.andre@gmail.com>
2026-08-08 04:33:00 +0200

    Remove unwanted comments in config/services.php

--- src/Pagination/config/services.php
+++ src/Pagination/config/services.php
@@ -18,17 +18,11 @@ use Symfony\UX\Pagination\PaginationInfoFormatter;
 use Symfony\UX\Pagination\Paginator;
 use Symfony\UX\Pagination\PaginatorInterface;
 
-/*
- * @author Simon André <smn.andre@gmail.com>
- */
-
 return static function (ContainerConfigurator $container): void {
     $services = $container->services()
         ->defaults()
             ->private()
     ;
-
-    // -- Adapters ------------------------------------------------------------
 
     $services->set('ux_pagination.cursor_codec', CursorCodec::class)
         ->lazy(CursorCodecInterface::class)
@@ -39,13 +33,9 @@ return static function (ContainerConfigurator $container): void {
         ->tag('ux_pagination.adapter')
     ;
 
-    // -- Utilities -----------------------------------------------------------
-
     $services->set('ux_pagination.info_formatter', PaginationInfoFormatter::class)
         ->arg('$translator', service('translator')->nullOnInvalid())
     ;
-
-    // -- Core ----------------------------------------------------------------
 
     $services->set('ux_pagination.paginator', Paginator::class)
         ->arg('$adapters', tagged_iterator('ux_pagination.adapter', defaultPriorityMethod: 'getDefaultPriority'))
